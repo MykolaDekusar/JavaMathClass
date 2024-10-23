f0a00d8e877ea47eb01e6b5386bc6122c2ca7dd6
MykolaDekusar <ndekusar97@gmail.com>
2024-10-23 16:55:01 +0200

    Asking user to input 2 triangle cathetus using GUI to get the hypotenuse

--- JavaMathClass/src/Main.java
+++ JavaMathClass/src/Main.java
@@ -1,8 +1,13 @@
-
+import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-
+		//calcoliamo l'ipotenusa di un triangolo dall'input dell'utente
+		JOptionPane.showMessageDialog(null, "Calcoliamo l'ipotenusa di un triangolo", null, 1);
+		double a = Float.parseFloat(JOptionPane.showInputDialog("Inserisci il primo cateto"));
+		double b= Float.parseFloat(JOptionPane.showInputDialog("Inserisci il secondo cateto"));
+		//usiamo Math.sqrt che sarebbe la radice quadrata di a^2 + b^2
+		double ipotenusa= Math.sqrt((a*a)+(b*b));
 	}
 
 }
